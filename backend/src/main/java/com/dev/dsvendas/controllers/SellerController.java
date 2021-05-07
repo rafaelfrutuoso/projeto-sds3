ca3a3baa20f1764e038ef581523a3297b90d168d
rafaelfrutuoso <rafaelfrutuoso@live.com>
2021-05-07 10:18:47 -0300

    Layers

---
+++ backend/src/main/java/com/dev/dsvendas/controllers/SellerController.java
@@ -0,0 +1,28 @@
+package com.dev.dsvendas.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.dsvendas.dto.SellerDTO;
+import com.dev.dsvendas.service.SellerService;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/sellers")
+public class SellerController {
+	
+	@Autowired
+	private SellerService service;
+	
+	@GetMapping
+	public ResponseEntity<List<SellerDTO>> findAll(){
+		List<SellerDTO> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+
+}
