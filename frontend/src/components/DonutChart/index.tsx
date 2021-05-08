d45a91b88defce454b3af6203d2342810641ef16
rafaelfrutuoso <rafaelfrutuoso@live.com>
2021-05-08 20:43:20 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale';
 import {BASE_URL} from 'utils/requests'
@@ -14,25 +15,23 @@ const DonutChart = () => {
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
     */
-    // Foma errada
-    let  chartData : ChartData = {labels:[], series:[]};
-
-    // busca os dados do banco
+    const [chartData, setChartData] = useState<ChartData>( {labels:[], series:[]})
+   
+    useEffect(() =>{
+         // busca os dados do banco
     axios.get(`${BASE_URL}sales/amount-by-seller`)
-           .then(response =>{
-               const data  = response.data as SaleSum[]
-               const myLabels =  data.map( x => x.sellerName)
-               const mySeries =  data.map( x => x.sum)
+    .then(response =>{
+        const data  = response.data as SaleSum[]
+        const myLabels =  data.map( x => x.sellerName)
+        const mySeries =  data.map( x => x.sum)
 
-               chartData = {labels:myLabels, series: mySeries};
+        setChartData({labels:myLabels, series: mySeries});
 
-               console.log(response.data);
-               
-           })
-    
-  
+        console.log(chartData);
+         
+    })
+    },[])
 
-    
     const options = {
         legend: {
             show: true
